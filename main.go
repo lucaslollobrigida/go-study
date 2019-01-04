15be73e824888bfb6abf60b9bc78a14bb6877138
Lucas Lollobrigida <lucas.lollobrigida@goomer.com.br>
2019-01-04 20:03:26 -0200

    rest: add router and basic logic

--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
-  "fmt"
+	"encoding/json"
+  "log"
+  "net/http"
   "github.com/gorilla/mux"
 )
 
 func main() {
-    fmt.Println("hello world")
+    router := mux.NewRouter()
+    router.HandleFunc("/hello/{name}", SayHello).Methods("GET")
+		router.HandleFunc("/", Welcome).Methods("GET")
+    log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func SayHello(w http.ResponseWriter, r *http.Request) {
+		params := mux.Vars(r)
+		message := "Hello, " + params["name"]
+		json.NewEncoder(w).Encode(message)
+}
+
+func Welcome(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode("Welcome")
 }
